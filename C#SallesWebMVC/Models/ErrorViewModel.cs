267bbed25a5d07fc2eba1a53210890eca11f5813
Caua-Girard <caua.girard2006@gmail.com>
2024-08-10 01:20:06 -0300

    Projeto Criado

---
+++ C#SallesWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C_SallesWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
