717becf1084ef02b7eb92c05e72147819dc11ee8
Caua-Girard <caua.girard2006@gmail.com>
2024-08-28 16:55:36 -0300

    Sellers controller

---
+++ C#SallesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C_SallesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
